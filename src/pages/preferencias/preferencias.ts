3be382c6f0b4c690a743cf616328d8de0e78354d
julianeborba <juliane.borba2@gmail.com>
2019-04-09 22:11:53 -0300

    retorno da lista de preferencias backend

--- src/pages/preferencias/preferencias.ts
+++ src/pages/preferencias/preferencias.ts
@@ -4,6 +4,7 @@ import { UsuarioPage } from '../usuario/usuario';
 import { ServidorProvider } from '../../providers/servidor/servidor';
 import { PreferenciasDTO } from '../models/preferencias';
 
+
 /**
  * Generated class for the PreferenciasPage page.
  *
@@ -20,45 +21,90 @@ export class PreferenciasPage {
 
 
   usuario: PreferenciasDTO = {
-    usuario_final:  JSON.parse(localStorage.getItem('usuario')),//localStorage.getItem('usuario'),
-    interesses: "" // new Array()
+    usuario_final: JSON.parse(localStorage.getItem('usuario')),//localStorage.getItem('usuario'),
+    interesses: new Array() //"" 
   };
 
+  interesses: any = [];
 
   constructor(public navCtrl: NavController, public toast: ToastController, public servidor: ServidorProvider, public navParams: NavParams) {
+
+  }
+
+  ionViewDidLoad() {
+    this.obterPreferencias();
+    console.log("ionViewDidLoad PreferenciasPage");
   
+
+  }
+  obterPreferencias() {
+    this.servidor.obterPreferencias().subscribe(sucess => {
+      this.interesses = sucess;
+      for (let i = 0; i < this.interesses.length; i++) {
+        this.interesses[i].checked = false;
+      }
+      this.servidor.obterPreferenciasPorUsuario(this.usuario.usuario_final).subscribe(
+        interessePorUsuario => {
+          for (let i = 0; i < interessePorUsuario.length; i++) {
+            for (let x = 0; x < this.interesses.lenght; x++) {
+              console.log(interessePorUsuario[i].id_interesse);
+              console.log(this.interesses[x].id_interesse);
+              // console.log(this.interesses);
+              if (interessePorUsuario[i].id_interesse == this.interesses[x].id_interesse) {
+                this.interesses[x].checked = true;
+                console.log(this.interesses);
+                break;
+              }
+            }
+          }
+        }
+      )
+
+    },
+      erro => {
+
+      });
   }
 
 
   salvarPreferencias() {
-    this.servidor.salvarPreferencias(this.usuario).subscribe(item => {
-      this.navCtrl.setRoot(UsuarioPage);
-      this.toast.create({
-        message: 'Cadastro de Preferências Realizado com Sucesso ', position: 'botton', duration: 3000
-      }).present();
-    }, error => {
-      this.toast.create({
-        message: "Erro ao realizar cadastro de preferências. Erro: " + error.error.message, position: 'botton', duration: 3000
-      }).present();
-    })
+    this.usuario.interesses = new Array();
+    for (let i = 0; i < this.interesses.length; i++) {
+      //  console.log(this.usuario.interesses); 
+      //  console.log('Checked ' + this.interesses[i].checked);
+      if (this.interesses[i].checked) {
+        this.usuario.interesses.push(this.interesses[i].id_interesse);
+      }
+    }
+    console.log(this.usuario.interesses);
 
+    this.servidor.salvarPreferencias(this.usuario).subscribe(
+      sucess => {
+        console.log(sucess)
+        this.navCtrl.setRoot(UsuarioPage);
+        this.toast.create({
+          message: 'Cadastro de Preferências Realizado com Sucesso ', position: 'botton', duration: 3000
+        }).present();
+      }, error => {
+        this.toast.create({
+          message: "Erro ao realizar cadastro de preferências. Erro: " + error.error.message, position: 'botton', duration: 3000
+        }).present();
+
+      })
   }
+  /* salvarPreferencias() {
+      this.servidor.salvarPreferencias(this.usuario).subscribe(item => {
+        this.navCtrl.setRoot(UsuarioPage);
+        this.toast.create({
+          message: 'Cadastro de Preferências Realizado com Sucesso ', position: 'botton', duration: 3000
+        }).present();
+      }, error => {
+        this.toast.create({
+          message: "Erro ao realizar cadastro de preferências. Erro: " + error.error.message, position: 'botton', duration: 3000
+        }).present();
+      })
+  
+   } */
 
- /* descricao: Array < { value: number, descricaoNome: string } > = [
-    { value: 1, descricaoNome: 'Música' },
-    { value: 2, descricaoNome: 'Esporte' },
-    { value: 3, descricaoNome: 'Carros' },
-    { value: 4, descricaoNome: 'Filmes' },
-    { value: 5, descricaoNome: 'Comida' },
-    { value: 6, descricaoNome: 'Bebida' },
-    { value: 7, descricaoNome: 'Política' },
-    { value: 8, descricaoNome: 'Eletronicos' },
-    { value: 9, descricaoNome: 'Viagens' },
-    { value: 10, descricaoNome: 'Educação' },
-    { value: 11, descricaoNome: 'Fofoca' },
-    { value: 12, descricaoNome: 'Internet' },
-    { value: 13, descricaoNome: 'Economia' },
-    { value: 14, descricaoNome: 'Livros' }
-  ];*/
 
 }
