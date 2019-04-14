732f2b8e9b4d45fd9665f2520ec8349d3740fa1c
adrianoprog <adrianojoseadr@gmail.com>
2019-04-14 16:32:00 -0300

    Limitação de caracteres no preenchimento do formulário de usuário

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,25 +1,23 @@
-import { CadastroPage } from './../cadastro/cadastro';
-import { Component } from '@angular/core';
-import { NavController, NavParams, AlertController, ToastController } from 'ionic-angular';
-import { Http } from '@angular/http';
-import { ServidorProvider } from '../../providers/servidor/servidor';
-import { UsuarioPage } from '../usuario/usuario'
+import { CadastroPage } from "./../cadastro/cadastro";
+import { Component } from "@angular/core";
+import {
+  NavController,
+  NavParams,
+  AlertController,
+  ToastController
+} from "ionic-angular";
+import { Http } from "@angular/http";
+import { ServidorProvider } from "../../providers/servidor/servidor";
+import { UsuarioPage } from "../usuario/usuario";
 
-
-import { CredenciaisDTO } from '../models/credenciais';
-import {Storage} from '@ionic/storage';
-
-
-
-
-
+import { CredenciaisDTO } from "../models/credenciais";
+import { Storage } from "@ionic/storage";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
   //usuario: any;
   isTextFieldType: boolean;
   //login_usuario: string;
@@ -30,12 +28,16 @@ export class HomePage {
     senha: ""
   };
 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage,
-    public alertCtrl: AlertController, public servidor: ServidorProvider, public http: Http, public toast: ToastController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private storage: Storage,
+    public alertCtrl: AlertController,
+    public servidor: ServidorProvider,
+    public http: Http,
+    public toast: ToastController
+  ) {
     //this.usuario = {};
-
   }
 
   togglePasswordFieldType() {
@@ -46,33 +48,41 @@ export class HomePage {
     this.navCtrl.push(CadastroPage);
   }
 
-
-
   logar() {
-    if (this.usuario.login_usuario == undefined || this.usuario.senha == undefined) {
+    if (
+      this.usuario.login_usuario == undefined ||
+      this.usuario.senha == undefined
+    ) {
       let alert = this.alertCtrl.create({
-        title: 'Atenção',
-        message: 'Preencha todos os campos!',
-        buttons: ['OK']
-      })
+        title: "Atenção",
+        message: "Preencha todos os campos!",
+        buttons: ["OK"]
+      });
       alert.present();
     } else {
       // return new Promise((resolve, reject) => {
-      this.servidor.logar(this.usuario).subscribe(response => {
-        //localStorage.setItem("usuario", this.usuario.login_usuario);
-        localStorage.setItem("usuario", JSON.stringify(this.usuario.login_usuario));
+      this.servidor.logar(this.usuario).subscribe(
+        response => {
+          //localStorage.setItem("usuario", this.usuario.login_usuario);
+          localStorage.setItem(
+            "usuario",
+            JSON.stringify(this.usuario.login_usuario)
+          );
 
-        this.navCtrl.setRoot(UsuarioPage);
-      }, error => {
-        this.toast.create({
-          message: "Erro ao realizar cadastro. Erro: " + error.error.message, position: 'botton', duration: 3000
-        }).present();
-      });
+          this.navCtrl.setRoot(UsuarioPage);
+        },
+        error => {
+          this.toast
+            .create({
+              message:
+                "Erro ao realizar cadastro. Erro: " + error.error.message,
+              position: "botton",
+              duration: 3000
+            })
+            .present();
+        }
+      );
       //});
-      }
     }
-  
+  }
 }
-
-
-
