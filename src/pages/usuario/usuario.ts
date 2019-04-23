ae1ded6858c1fffc6dc4cd54c211291e79b2826a
Eleuterio <adriano.j.eleuterio@accenture.com>
2019-04-23 10:42:57 -0300

    primeiro comite branch cep

--- src/pages/usuario/usuario.ts
+++ src/pages/usuario/usuario.ts
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
-import { Component, OnInit } from "@angular/core";
-import { NavController, NavParams } from "ionic-angular";
-=======
 import { HomePage } from "./../home/home";
 import { Component, OnInit } from "@angular/core";
 import { NavController, NavParams, ModalController } from "ionic-angular";
->>>>>>> b315695777a73d761ee6fbfc9ae20f5659f4e5e3
 import { ServidorProvider } from "../../providers/servidor/servidor";
-
 import { Storage } from "@ionic/storage";
 import { PreferenciasPage } from "../preferencias/preferencias";
-<<<<<<< HEAD
-import { HomePage } from "../home/home";
-import { CredenciaisDTO } from "../models/credenciais";
-import { CadastroPage } from "../cadastro/cadastro";
-import { JsonPipe } from "@angular/common";
-=======
 import { CredenciaisDTO } from "../models/credenciais";
 import { CadastroPage } from "../cadastro/cadastro";
 import { PontuacaoDTO } from "../models/pontos";
 import { EditarPage } from "../editar/editar";
->>>>>>> b315695777a73d761ee6fbfc9ae20f5659f4e5e3
 
 @Component({
   selector: "page-usuario",
@@ -32,11 +19,7 @@ export class UsuarioPage {
     senha: ""
   };
 
-<<<<<<< HEAD
-  pontuacao: any;
-=======
   pontuacao: string;
->>>>>>> b315695777a73d761ee6fbfc9ae20f5659f4e5e3
 
   constructor(
     public navCtrl: NavController,
@@ -66,15 +49,6 @@ export class UsuarioPage {
     this.servidor
       .obterPontuacaoPorUsuario(this.usuario.login_usuario)
       .subscribe(pontuacaoPorUsuario => {
-<<<<<<< HEAD
-        console.log("pontuação do usuario" + pontuacaoPorUsuario);
-        this.pontuacao = pontuacaoPorUsuario;
-        console.log("Data loo", this.usuario.login_usuario);
-        console.log("Data loo", this.pontuacao);
-      });
-  }
-
-=======
         console.log(pontuacaoPorUsuario);
 
         this.pontuacao = JSON.stringify(pontuacaoPorUsuario);
@@ -86,7 +60,6 @@ export class UsuarioPage {
       });
   }
 
->>>>>>> b315695777a73d761ee6fbfc9ae20f5659f4e5e3
   sair() {
     window.localStorage.removeItem("usuario");
     this.navCtrl.setRoot(HomePage);
